a956aa4177434ebe82eb37235ff463add6e58cb9
alelaht <lahtinen.ale@gmail.com>
2017-05-03 11:10:55 +0000

    Fix sensori.py and DBconnect.py

--- sensori.py
+++ sensori.py
@@ -1,13 +1,17 @@
 from gpiozero import MotionSensor
 from picamera import PiCamera
 from datetime import datetime
+import DBconnect
 import time
+import subprocess
 
 camera = PiCamera()
 pir = MotionSensor(4)
 while True:
 	if pir.motion_detected:
-		filename = datetime.now().strftime("%Y-%d-%m_%H.%M.%S.jpeg")
+		filename = datetime.now().strftime("%Y-%m-%d_%H.%M.%S.jpeg")
 		camera.capture(filename)
+		p = subprocess.Popen(["scp", filename, "pi@10.207.3.236:/home/pi/kuvat"])
 		print("Motion detected!")
+		DBconnect.connect()
 	time.sleep(1)
